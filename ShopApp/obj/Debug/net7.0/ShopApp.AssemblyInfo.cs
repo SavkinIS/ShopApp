89c53fe180f8bd34dfd77e7ed5c81ce2fe304160
Ivan Savkin <i.savkin@mysterytag.com>
2025-03-23 17:45:28 +0300

    Update product

--- ShopApp/obj/Debug/net7.0/ShopApp.AssemblyInfo.cs
+++ ShopApp/obj/Debug/net7.0/ShopApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4ea4d692eab9c509554ac42039c987aab21528")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93662731e061e492a21749f314da8c4e63cfbe7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
